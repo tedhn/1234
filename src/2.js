0bf67f7f72b3443f5381e92b9831ee30d4d0c4fb
Ted <heinhtetnaing186@gmail.com>
2019-11-25 20:21:14 +0630

    'asdfasdf'

--- src/2.js
+++ src/2.js
@@ -1,15 +1,13 @@
 import React , {useState, useEffect} from 'react'
 import WOW from 'wow.js'
 
+export default function B(){
 
-export default function B(prop){
 
 	return(
 		<div className='Container2'>
 
-
-				<div className='title'>About Me</div>
-
+			<div className='wow fadeInUp title'>About Me</div>
 
 			<div className='wow fadeInUp'>
 				<div className='Content'>Just a dumb 20 year old doing dumber things with his life :D</div>
